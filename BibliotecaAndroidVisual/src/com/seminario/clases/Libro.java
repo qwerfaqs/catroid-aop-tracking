6ce94f381126d7c155a44b2c550b3f2388adca8d
qwerfaqs <qwerfaqs@gmail.com>
2011-09-26 22:56:51 +0000

    Agregue el equals y el hashCode suponiendo la existencia de os metods getters getTitulo() getAutor() getDescripcion()

--- BibliotecaAndroidVisual/src/com/seminario/clases/Libro.java
+++ BibliotecaAndroidVisual/src/com/seminario/clases/Libro.java
@@ -12,4 +12,25 @@ public class Libro {
 		this.autor = autor;
 		this.descripcion=descripcion;
 	}
+     public boolean equals(Object other) {
+        if (this == other) return true;
+        if ( !(other instanceof Libro) ) return false;
+
+        final Libro  libro = (Libro) libre;
+
+        if ( !cat.getTitulo().equals( getTitulo() ) ) return false;
+        if ( !cat.getAutor().equals( getAutor() ) ) return false;
+
+        return true;
+    }
+
+    public int hashCode() {
+        int result;
+        result = getTitulo().hashCode();
+        result = 29 * result + getTitulo().hashCode();
+        result = 29 * result + getAutor().hashCode();
+        result = 29 * result + getDescripcion().hashCode();
+        return result;
+    }
+
 }
