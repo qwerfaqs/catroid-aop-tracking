187e6c2ff3060cdb819754c34a480afca8ba284a
Juan <Juan@Juan-PC>
2011-09-26 23:52:47 -0300

    Lo vuelvo a subir porque me tiraba error en ejecuci?

--- BibliotecaAndroidVisual/gen/com/seminario/R.java
+++ BibliotecaAndroidVisual/gen/com/seminario/R.java
@@ -8,17 +8,43 @@
 package com.seminario;
 
 public final class R {
+    public static final class array {
+        public static final int libros_autor_array=0x7f050001;
+        public static final int libros_descripcion_array=0x7f050002;
+        public static final int libros_nombre_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int btnIrListaLibro=0x7f060008;
+        public static final int btnMeGustaLibro=0x7f060006;
+        public static final int btnNoMeGusta=0x7f060007;
+        public static final int txtAutor=0x7f060002;
+        public static final int txtAutorLibro=0x7f060003;
+        public static final int txtDescripcion=0x7f060004;
+        public static final int txtDescripcionLibro=0x7f060005;
+        public static final int txtTitulo=0x7f060000;
+        public static final int txtTituloLibro=0x7f060001;
+    }
     public static final class layout {
-        public static final int lista_libro=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int descripcion_libro=0x7f030000;
+        public static final int lista_libro=0x7f030001;
+        public static final int main=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int btnIrListaLibro=0x7f04000a;
+        public static final int btnMeGustaLibro=0x7f040008;
+        public static final int btnNoMeGusta=0x7f040009;
         public static final int hello=0x7f040000;
+        public static final int txtAutor=0x7f040006;
+        public static final int txtAutorLibro=0x7f040007;
+        public static final int txtDescripcion=0x7f040002;
+        public static final int txtDescripcionLibro=0x7f040003;
+        public static final int txtTitulo=0x7f040005;
+        public static final int txtTituloLibro=0x7f040004;
     }
 }
