88d884ad3a325e7bb3df673143a1ed430c736cb3
qwerfaqs <qwerfaqs@gmail.com>
2011-11-23 02:16:30 -0300

    Se borraron los botones del detalle, lo que da mas espacio como para poner la tapa del libro.

--- BibliotecaAndroidVisual/gen/com/seminario/R.java
+++ BibliotecaAndroidVisual/gen/com/seminario/R.java
@@ -19,20 +19,24 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int btnIrListaLibro=0x7f060008;
-        public static final int btnMeGustaLibro=0x7f060006;
-        public static final int btnNoMeGustaLibro=0x7f060007;
-        public static final int txtAutor=0x7f060002;
-        public static final int txtAutorLibro=0x7f060003;
-        public static final int txtDescripcion=0x7f060004;
-        public static final int txtDescripcionLibro=0x7f060005;
-        public static final int txtTitulo=0x7f060000;
-        public static final int txtTituloLibro=0x7f060001;
+        public static final int opcionConfig=0x7f070008;
+        public static final int opcionMegusta=0x7f070006;
+        public static final int opcionNoMeGusta=0x7f070007;
+        public static final int txtAutor=0x7f070002;
+        public static final int txtAutorLibro=0x7f070003;
+        public static final int txtDescripcion=0x7f070004;
+        public static final int txtDescripcionLibro=0x7f070005;
+        public static final int txtTitulo=0x7f070000;
+        public static final int txtTituloLibro=0x7f070001;
     }
     public static final class layout {
         public static final int descripcion_libro=0x7f030000;
         public static final int lista_libro=0x7f030001;
         public static final int main=0x7f030002;
+    }
+    public static final class menu {
+        public static final int descripcion_menu=0x7f060000;
+        public static final int listado_menu=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
@@ -40,6 +44,8 @@ public final class R {
         public static final int btnMeGustaLibro=0x7f040008;
         public static final int btnNoMeGustaLibro=0x7f040009;
         public static final int hello=0x7f040000;
+        public static final int opcionConfig=0x7f04000b;
+        public static final int opcionConfigCondensed=0x7f04000c;
         public static final int txtAutor=0x7f040006;
         public static final int txtAutorLibro=0x7f040007;
         public static final int txtDescripcion=0x7f040002;
--- BibliotecaAndroidVisual/src/com/seminario/ListadoLibro.java
+++ BibliotecaAndroidVisual/src/com/seminario/ListadoLibro.java
@@ -3,6 +3,8 @@ package com.seminario;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -13,36 +15,57 @@ import android.widget.TextView;
 import com.seminario.controladoras.LibroControladora;
 
 public class ListadoLibro extends ListActivity {
-    /** Called when the activity is first created. */
-	
+	/** Called when the activity is first created. */
+
 	LibroControladora libroControladora;
-	
+
 	public ListadoLibro() {
 		libroControladora = new LibroControladora();
 	}
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.lista_libro,libroControladora.devolverLibrosNombre()));
-        
-        ListView lv = getListView();
-        lv.setTextFilterEnabled(true);
-        
-        lv.setOnItemClickListener(new OnItemClickListener() {
 
-			public void onItemClick(AdapterView<?> parent, View view, int posicion, long id) {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.lista_libro,
+				libroControladora.devolverLibrosNombre()));
+
+		ListView lv = getListView();
+		lv.setTextFilterEnabled(true);
+
+		lv.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parent, View view,
+					int posicion, long id) {
 				// Ir a la otra actividad pasando el libro seleccionado{
-				Intent intento = new Intent(ListadoLibro.this, DescripcionLibro.class);
-				
-				//Libro aux = libroControladora.devolverLibro(((TextView)view).getText().toString());//recuperar el id con el titulo del libro, no hace falta ya que no existen libros con el mismo nombre
-				
-				intento.putExtra("nombreLibro"/*"idLibro"*/, ((TextView)view).getText().toString()/*aux.getId()*/);//pasar como extra el id... Corrección pasar el nombre del libro
+				Intent intento = new Intent(ListadoLibro.this,
+						DescripcionLibro.class);
+
+				// Libro aux =
+				// libroControladora.devolverLibro(((TextView)view).getText().toString());//recuperar
+				// el id con el titulo del libro, no hace falta ya que no
+				// existen libros con el mismo nombre
+
+				intento.putExtra("nombreLibro"/* "idLibro" */, ((TextView) view)
+						.getText().toString()/* aux.getId() */);// pasar como
+																// extra el
+																// id...
+																// Corrección
+																// pasar el
+																// nombre del
+																// libro
 				startActivity(intento);
-				//}otra actividad
-				
+				// }otra actividad
+
 			}
 		});
-    }
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.listado_menu, menu);
+		return true;
+	}
 }
