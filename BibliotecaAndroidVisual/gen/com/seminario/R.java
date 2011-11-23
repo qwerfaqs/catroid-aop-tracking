26a2ad22d75287389d8ed31c78843ee38fd86cda
qwerfaqs <qwerfaqs@gmail.com>
2011-11-23 03:43:27 -0300

    Saque el boton de las vista y llame a la configuracion desde el menu de opciones, con icono y todo.

--- BibliotecaAndroidVisual/gen/com/seminario/R.java
+++ BibliotecaAndroidVisual/gen/com/seminario/R.java
@@ -19,12 +19,11 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int img_btn_configuracion=0x7f070007;
         public static final int linearLayout1=0x7f070006;
-        public static final int list_libros=0x7f070008;
-        public static final int opcionConfig=0x7f07000b;
-        public static final int opcionMegusta=0x7f070009;
-        public static final int opcionNoMeGusta=0x7f07000a;
+        public static final int list_libros=0x7f070007;
+        public static final int opcionConfig=0x7f07000a;
+        public static final int opcionMegusta=0x7f070008;
+        public static final int opcionNoMeGusta=0x7f070009;
         public static final int txtAutor=0x7f070002;
         public static final int txtAutorLibro=0x7f070003;
         public static final int txtDescripcion=0x7f070004;
--- BibliotecaAndroidVisual/src/com/seminario/ListadoLibroMasConfiguracion.java
+++ BibliotecaAndroidVisual/src/com/seminario/ListadoLibroMasConfiguracion.java
@@ -5,87 +5,108 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.seminario.controladoras.LibroControladora;
 
-public class ListadoLibroMasConfiguracion extends Activity{
+public class ListadoLibroMasConfiguracion extends Activity {
 
 	private ListView listLibros;
-	private ImageButton imgBtnConfiguracion;
-	
+
 	private boolean filtroSeguimiento = true;
-	
+
 	private LibroControladora libroControladora;
-	
-	
+
 	public ListadoLibroMasConfiguracion() {
 		libroControladora = new LibroControladora();
 	}
-	
-	private void levantarXml()
-	{
-		listLibros  = (ListView)findViewById(R.id.list_libros);
-		imgBtnConfiguracion = (ImageButton)findViewById(R.id.img_btn_configuracion);
+
+	private void levantarXml() {
+		listLibros = (ListView) findViewById(R.id.list_libros);
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.lista_libro_mas_configuracion);
 		levantarXml();
-		
-		ArrayAdapter<String> nombres = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,libroControladora.devolverLibrosNombre());
+
+		ArrayAdapter<String> nombres = new ArrayAdapter<String>(
+				getApplicationContext(), android.R.layout.simple_list_item_1,
+				libroControladora.devolverLibrosNombre());
 		listLibros.setAdapter(nombres);
-		
-		listLibros.setOnItemClickListener(new OnItemClickListener()
-		{
+
+		listLibros.setOnItemClickListener(new OnItemClickListener() {
 
 			public void onItemClick(AdapterView<?> arg0, View view, int pos,
 					long id) {
 				// Ir a la otra actividad pasando el libro seleccionado{
-				Intent intento = new Intent(ListadoLibroMasConfiguracion.this, DescripcionLibro.class);
-				
-				//Libro aux = libroControladora.devolverLibro(((TextView)view).getText().toString());//recuperar el id con el titulo del libro, no hace falta ya que no existen libros con el mismo nombre
-				
-				intento.putExtra("nombreLibro", ((TextView)view).getText().toString());//pasar como extra el id... Corrección pasar el nombre del libro
+				Intent intento = new Intent(ListadoLibroMasConfiguracion.this,
+						DescripcionLibro.class);
+
+				// Libro aux =
+				// libroControladora.devolverLibro(((TextView)view).getText().toString());//recuperar
+				// el id con el titulo del libro, no hace falta ya que no
+				// existen libros con el mismo nombre
+
+				intento.putExtra("nombreLibro", ((TextView) view).getText()
+						.toString());// pasar como extra el id... Corrección
+										// pasar el nombre del libro
 				startActivity(intento);
-				//}otra actividad
-				
+				// }otra actividad
+
 			}
-			
+
 		});
-		
-		imgBtnConfiguracion.setOnClickListener(new OnClickListener()
-		{
-			public void onClick(View v) {
-				AlertDialog.Builder builder = new AlertDialog.Builder(ListadoLibroMasConfiguracion.this);
-				builder.setTitle("Configuración");
-				//en un futuro se pueden agregar más opciones
-				final CharSequence[] items = { "Seguimiento" }; 
-				final boolean[] states = { filtroSeguimiento };
-				builder.setMultiChoiceItems(items, states, new DialogInterface.OnMultiChoiceClickListener() {
-					
-					public void onClick(DialogInterface dialog, int item, boolean estado) {
-						if(item == 0)
-						{
+
+
+
+	}
+	public void mostrarConfiguracion() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(
+				ListadoLibroMasConfiguracion.this);
+		builder.setTitle("Configuración");
+		// en un futuro se pueden agregar más opciones
+		final CharSequence[] items = { "Seguimiento" };
+		final boolean[] states = { filtroSeguimiento };
+		builder.setMultiChoiceItems(items, states,
+				new DialogInterface.OnMultiChoiceClickListener() {
+
+					public void onClick(DialogInterface dialog,
+							int item, boolean estado) {
+						if (item == 0) {
 							filtroSeguimiento = estado;
-							//aca se podría desactivar el seguimiento
+							// aca se podría desactivar el seguimiento
 						}
 					}
 				});
-				AlertDialog d = builder.create();
-				d.show();
-			}
-		});
-		
+		AlertDialog d = builder.create();
+		d.show();
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.listado_menu, menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection
+	    switch (item.getItemId()) {
+	    case R.id.opcionConfig:
+	    	mostrarConfiguracion();
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
 	}
 }
