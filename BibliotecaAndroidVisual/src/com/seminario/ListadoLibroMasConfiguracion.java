65e8a434b4ee5e0dce6e10c44a6c1fdaaebdab22
Juan <Juan@Juan-PC>
2011-11-28 17:31:46 -0300

    BibliotecaAndroidVisual: La configuraci?n del seguimiento ahora se guarda en BD Sqlite.

--- BibliotecaAndroidVisual/src/com/seminario/ListadoLibroMasConfiguracion.java
+++ BibliotecaAndroidVisual/src/com/seminario/ListadoLibroMasConfiguracion.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,42 +19,35 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.seminario.controladoras.LibroControladora;
+import com.seminario.controladoras.SeguimentoControladora;
+import com.seminario.daos.SeguimientoDao;
 
 public class ListadoLibroMasConfiguracion extends Activity {
 
 	private ListView listLibros;
 
-	private boolean filtroSeguimiento = true;
-	private SharedPreferences pref; //preferencias
-	SharedPreferences.Editor editor;
+	private boolean filtroSeguimientoUsuario;
 
 	private LibroControladora libroControladora;
+	private SeguimentoControladora seguimentoControladora;
 
 	public ListadoLibroMasConfiguracion() {
 		libroControladora = new LibroControladora();
+		seguimentoControladora = new SeguimentoControladora(this, "seguimiento", null, 1);
 	}
 
 	private void levantarXml() {
 		listLibros = (ListView) findViewById(R.id.list_libros);
 	}
-
-	public boolean guardarSeguimiento(boolean seguimiento) {
-		this.filtroSeguimiento = seguimiento;
-		editor = pref.edit();
-		editor.putBoolean("seguimiento", seguimiento);
-		editor.commit();
-		return seguimiento;
-	}
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		pref = PreferenceManager.getDefaultSharedPreferences(this);//preferencias, puede ir en el onCreate()
-		filtroSeguimiento = pref.getBoolean("seguimiento", false);//recuperar seguimiento almacenado, puede ir en el onCreate()
-		
 		setContentView(R.layout.lista_libro_mas_configuracion);
 		levantarXml();
+		
+		seguimentoControladora.insertarSeguimientoUsuario(true);//valor inicial
+        filtroSeguimientoUsuario = seguimentoControladora.devolverSeguimientoUsuario();
 
 		ArrayAdapter<String> nombres = new ArrayAdapter<String>(
 				getApplicationContext(), android.R.layout.simple_list_item_1,
@@ -92,14 +86,15 @@ public class ListadoLibroMasConfiguracion extends Activity {
 		builder.setTitle("Configuración");
 		// en un futuro se pueden agregar más opciones
 		final CharSequence[] items = { "Seguimiento" };
-		final boolean[] states = { filtroSeguimiento };
+		final boolean[] states = { filtroSeguimientoUsuario };
 		builder.setMultiChoiceItems(items, states,
 				new DialogInterface.OnMultiChoiceClickListener() {
 
 					public void onClick(DialogInterface dialog,
 							int item, boolean estado) {
 						if (item == 0) {
-							filtroSeguimiento = guardarSeguimiento(estado);
+							seguimentoControladora.actualizarSeguimientoUsuario(estado);
+							filtroSeguimientoUsuario = seguimentoControladora.devolverSeguimientoUsuario();
 							// aca se podría desactivar el seguimiento
 						}
 					}
---
+++ BibliotecaAndroidVisual/src/com/seminario/controladoras/SeguimentoControladora.java
@@ -0,0 +1,49 @@
+package com.seminario.controladoras;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.util.Log;
+
+import com.seminario.daos.SeguimientoDao;
+
+public class SeguimentoControladora {
+
+	private SeguimientoDao seguimientoDao;
+	public SeguimentoControladora(Context context, String name, CursorFactory factory, int version) {
+		seguimientoDao = new SeguimientoDao(context, name, factory, version);
+	}
+	
+	public void insertarSeguimientoUsuario(boolean estado)
+	{
+		//Log.v("seminario", "vacioSeguimiento: "+String.valueOf(seguimientoDao.existeElementoSeguimientoUsuario()));
+		if(!seguimientoDao.existeElementoSeguimientoUsuario())
+		{
+			seguimientoDao.insertarSeguimientoUsuario(estado);
+		}
+		//else tendría que tirar una exepcion
+	}
+	
+	public void actualizarSeguimientoUsuario(boolean estado)
+	{
+		seguimientoDao.actualizarSeguimientoUsuario(estado);
+	}
+	
+	public boolean devolverSeguimientoUsuario()
+	{
+		return seguimientoDao.devolverSeguimientoUsuario();
+	}
+	
+	public void insertarSeguimientoServidor(boolean estado)
+	{
+		//Log.v("seminario", "vacioSeguimiento: "+String.valueOf(seguimientoDao.existeElementoSeguimientoUsuario()));
+		if(!seguimientoDao.existeElementoSeguimientoServidor())
+		{
+			seguimientoDao.insertarSeguimientoServidor(estado);
+		}
+	}
+	
+	public boolean devolverSeguimientoServidor()
+	{
+		return seguimientoDao.devolverSeguimientoServidor();
+	}
+}
